42a04979d01df4057d2978ef1f1a5fe6683c63d4
Pengsha Ying <yingfusheng@foxmail.com>
2022-03-14 20:03:41 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,35 @@
-import { createElement, Fragment, createComponent, propTypes } from 'axii'
+import {
+  createElement,
+  Fragment,
+  createComponent,
+  propTypes,
+  atom,
+} from "axii";
 
-App.propTypes = {
-  
-}
+App.propTypes = {};
 
-function App () {
+function App() {
+  const toggleValue = atom(false);
+
   return (
-    <div>
-      This is axii-reproduce
-    </div>
-  )
+    <>
+      <button
+        onClick={() => {
+          toggleValue.value = !toggleValue.value;
+        }}
+      >
+        toggle
+      </button>
+      {() => toggleValue.value}
+      {() => (
+        <div style={toggleValue.value || { transform: "rotate(-90deg)" }}>
+          This is axii-reproduce
+        </div>
+      )}
+    </>
+  );
 }
 
-App.Style = (fragments) => {
+App.Style = (fragments) => {};
 
-}
-
-export default createComponent(App)
+export default createComponent(App);
