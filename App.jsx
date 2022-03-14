f1081c8a4e95358ee605d1fb135465a031b15696
Pengsha Ying <yingfusheng@foxmail.com>
2022-03-14 19:25:30 +0800

    add App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { createElement, Fragment, createComponent, propTypes } from 'axii'
+
+App.propTypes = {
+  
+}
+
+function App () {
+  return (
+    <div>
+      This is axii-reproduce
+    </div>
+  )
+}
+
+App.Style = (fragments) => {
+
+}
+
+export default createComponent(App)
